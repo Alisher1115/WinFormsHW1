3cc347f0799ba4c353699b9514978dde24f321d0
Мухамедин Алишер Серикович <@corp.itstep>
2018-12-08 19:30:44 +0600

    Added a new HW

---
+++ MyDb/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.ConnectionString = "server=localhost;user id=corp/chert;password= 0000;initial catalog=MyDB";
+            builder.AsynchronousProcessing = true;
+        }
+    }
+}
---
+++ MyDb/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyDb
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //            Создать базу данных MyDB.
+            //Создать простейшее приложение WinForms, позволяющее пользователю
+            //подключаться к базе данных MyDB, используя аутентификацию SQL
+            //Server.Для построения строки подключения использовать
+            //SqlConnectionStringBuilder.
+            string commandText;
+            SqlConnection connection = new SqlConnection("Server=localhost;Integrated security=SSPI;database=master");
+
+            commandText = "CREATE DATABASE MyDB";
+
+            SqlCommand command = new SqlCommand(commandText, connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                MessageBox.Show("DataBase 'MyDB' is Created Successfully", "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
